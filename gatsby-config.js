e55c55a65b82b7970e715bd2da9d9187a2ab7402
unknown <sergio-caballero_student2021@wilder.school>
2021-07-22 14:29:46 +0200

    added about us and homecontent getting data from datocms

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-datocms',
+      options: {
+        apiToken: 'a2eebac27f36d4fb84383a7fccb68a'
+      }
+    },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../components/layout"
 import ImageHotel from '../components/imageHotel';
+import HomeContent from "../components/homeContent";
 
 
 const IndexPage = () => (
   <Layout>
     <ImageHotel/>
+    <HomeContent/>
       
   </Layout>
 )
