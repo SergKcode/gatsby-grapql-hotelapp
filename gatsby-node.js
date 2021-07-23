78043b29943530374743a8f12ce615ae38bc10e6
unknown <sergio-caballero_student2021@wilder.school>
2021-07-23 19:10:58 +0200

    added rooms description

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,32 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const result = await graphql(`
+        query {
+            allDatoCmsRoom {
+                nodes {
+                    slug
+                }
+            }
+        }
+    `);
 
-// You can delete this file if you're not using it
+    // console.log(resultado.data.allDatoCmsHabitacion.nodes);
+
+    if(result.errors) {
+        reporter.panic('There is any result', result.errors);
+    }
+
+    // If there is pages, create files
+    const rooms = result.data.allDatoCmsRoom.nodes;
+
+    rooms.forEach(room => {
+        actions.createPage({
+            path: room.slug,
+            component: require.resolve('./src/components/rooms.js'),
+            context: {
+                slug: room.slug
+            }
+        })
+    })
+
+    
+}
